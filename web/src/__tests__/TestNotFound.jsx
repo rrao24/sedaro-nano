27ffcbc623381501a84462b980dec47ac90b6b44
rrao24 <rao.ram5718@gmail.com>
2025-05-12 18:04:35 -0400

    Simple unit tests

---
+++ web/src/__tests__/TestNotFound.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import NotFound from '../NotFound';
+
+test("Not Found renders successfully", () => {
+    render(<NotFound/>);
+
+    const element = screen.getByText(/404: Not Found/i);
+
+    expect(element).toBeInTheDocument();
+})
